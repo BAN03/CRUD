cdcfd920b23d49d39f29bf95dc568d5718b51fff
BAN03 <angeldejesusatanasiodiaz@gmail.com>
2024-11-15 02:16:19 -0600

    stable at the moment

--- src/main/java/com/ban03/TablesController.java
+++ src/main/java/com/ban03/TablesController.java
@@ -15,8 +15,8 @@ public class TablesController {
     @FXML
     TableView<DataRow> tableView = new TableView<>();
     private ObservableList<DataRow> data;
-    private String table;
-    private String SQL_SELECT;
+    private String table, columnNames = "", SQL_SELECT;
+    private String[] toCheck;
 
     public TablesController(String table) {
         this.table = table;
@@ -48,25 +48,33 @@ public class TablesController {
 
     private void cargar() throws SQLException {
         data.clear();
+        tableView.getColumns().clear();
         try (
                 Connection conn = new DBConecction().DBC();
                 Statement statement = conn.createStatement();
                 ResultSet resultSet = statement.executeQuery(this.SQL_SELECT)) {
             ResultSetMetaData metaData = resultSet.getMetaData();
             int columnCount = metaData.getColumnCount();
+            this.toCheck = new String[columnCount];
             for (int i = 1; i <= columnCount; i++) {
                 final int columnIndex = i - 1;
                 TableColumn<DataRow, String> column = new TableColumn<>(metaData.getColumnName(i));
+                columnNames += i > 1 ? ", " + metaData.getColumnName(i)
+                        : metaData.getColumnName(i);
                 column.setCellValueFactory(
                         cellData -> cellData.getValue().getValues().get(columnIndex));
                 column.setCellFactory(TextFieldTableCell.forTableColumn());
                 column.setOnEditCommit(event -> {
                     try {
-                        String query = "UPDATE " + this.table + " SET "
-                                + metaData.getColumnName(event.getTablePosition().getColumn() + 1) + " = "
-                                + "'" + event.getNewValue() + "'" + " WHERE " + metaData.getColumnName(1) + " = "
-                                + event.getRowValue().getValues().get(0).getValue();
-                        new DBConecction().DBC().createStatement().execute(query);
+                        if (event.getRowValue().getValues().get(0).getValue().compareTo("0") > 0) {
+                            String query = "UPDATE " + this.table + " SET "
+                                    + metaData.getColumnName(event.getTablePosition().getColumn() + 1) + " = "
+                                    + "'" + event.getNewValue() + "'" + " WHERE " + metaData.getColumnName(1) + " = "
+                                    + event.getRowValue().getValues().get(0).getValue();
+                            new DBConecction().DBC().createStatement().execute(query);
+                        } else {
+                            this.toCheck[event.getTablePosition().getColumn()] = event.getNewValue();
+                        }
                     } catch (SQLException e) {
                         System.out.println(e.getMessage());
                     }
@@ -87,6 +95,34 @@ public class TablesController {
     }
 
     @FXML
+    public void agregar() {
+        DataRow newRow;
+        ObservableList<SimpleStringProperty> values = FXCollections.observableArrayList();
+        for (int i = 1; i <= tableView.getColumns().size(); i++) {
+            values.add(new SimpleStringProperty(""));
+        }
+        newRow = new DataRow(values);
+        data.add(newRow);
+    }
+
+    @FXML
+    public void guardar() {
+        try {
+            int id = Integer.parseInt(this.data.get(this.data.size() - 2).getValues().get(0).getValue()) + 1;
+            String toInsert = "";
+            for (int i = 1; i < this.toCheck.length; i++) {
+                toInsert += ", " + "'" + this.toCheck[i] + "'";
+            }
+            String query = "INSERT INTO " + this.table + " (" + this.columnNames + ") VALUES (" + "'" + id + "'"
+                    + toInsert + ")";
+            new DBConecction().DBC().createStatement().execute(query);
+            cargar();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
     public void eliminar() {
 
         String query = "DELETE FROM " + this.table + " WHERE " + this.tableView.getColumns().get(0).getText() + " = "
@@ -103,5 +139,4 @@ public class TablesController {
     protected void salir(ActionEvent e) {
         ((Node) (e.getSource())).getScene().getWindow().hide();
     }
-
 }
